108745ab284cd29636d4297c90c003425d908324
Atharva Thakur <atharvathakur369@gmail.com>
2024-04-24 16:53:39 +0000

    Lottie animation added

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import time
 import pickle
 import numpy as np
 import pandas as pd
+import requests
 import streamlit as st
+from streamlit_lottie import st_lottie
 import matplotlib.pyplot as plt
 from streamlit_shap import st_shap
 from Modules.data_cleaning import cleaned_data
@@ -28,8 +30,22 @@ def stream_data(words):
         yield word + " "
         time.sleep(0.02)
 
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+# --- LOAD ASSETS---
+lottie_transaction = load_lottieurl("https://lottie.host/98295178-0a8f-4162-9a00-265d3f498cfd/SL87hpUcLF.json")
+
 def main():
-    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+    left, right = st.columns([2,1])
+    with left:
+        uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+    with right:
+        st_lottie(lottie_transaction, height=200, width=200, key="cash transaction")
+
     if uploaded_file:
         transaction_ids, modified_data = load_and_clean_data(uploaded_file)
         st.write(modified_data)
@@ -64,8 +80,8 @@ def main():
                 try:
                     with st.spinner('Analyzing...'):
                         response = generate_response(model, modified_data, idx, prediction[idx])
-                        st.write(response)
-                        # st.write_stream(stream_data(response))
+                        # st.write(response)
+                        st.write_stream(stream_data(response))
                 except Exception as e:
                     st.error(f"An error occurred: {e}")
     else:
